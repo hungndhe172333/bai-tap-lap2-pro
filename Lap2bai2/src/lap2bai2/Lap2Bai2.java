c3df7753347b000d6a5b3c2f12904f2223c65e59
hungndhe172333 <134671061+hungndhe172333@users.noreply.github.com>
2023-05-28 11:09:36 +0700

    Update Lap2Bai2.java

--- Lap2bai2/src/lap2bai2/Lap2Bai2.java
+++ Lap2bai2/src/lap2bai2/Lap2Bai2.java
@@ -1,9 +1,25 @@
 
 package lap2bai2;
-
+import java.util.Scanner;
 public class Lap2Bai2 {
+
     public static void main(String[] args) {
-        
+        boolean cout = false;
+        do {
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter the string: ");
+                String tenLop = sc.next();
+                if ( tenLop.matches("SE(.*)") == false )
+                    throw new Exception();
+                System.out.println("The string is " + tenLop);
+                cout = false;
+            }
+            catch( Exception e ){
+                System.out.println("The string is invalid");
+                cout = true;
+            }
+        }
+        while ( cout );          
     }
-    
 }
